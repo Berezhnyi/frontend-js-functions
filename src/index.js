0095b7aa63086ba5dd2e26800921547ad8ed5747
Berezhnyi <kivin13@gmail.com>
2019-05-24 21:17:05 +0300

    final(hope)

--- src/index.js
+++ src/index.js
@@ -1,53 +1,51 @@
-// export function min (array) {
-// 	if (typeof array !== 'undefined') {
-// 		let minimumnNumer = array[0];
-// 		let element;
+export function min (array) {
+	if (typeof array !== 'undefined') {
+		let minimumnNumer = array[0];
+		let element;
 
-// 		for (element of array) {
-// 			if (typeof element === 'number' && minimumnNumer > element) {
-// 				minimumnNumer = element;
-// 			}
-// 		}
-// 		return minimumnNumer;
-// 	}
-// }
-
-// export function max (array) {
-// 	if (typeof array !== 'undefined') {
-// 		let minimumnNumer = array[0];
-// 		let element;
-
-// 		for (element of array) {
-// 			if (typeof element === 'number' && minimumnNumer < element) {
-// 				minimumnNumer = element;
-// 			}
-// 		}
-// 		return minimumnNumer;
-// 	}
-// 	return undefined;
-// }
-
-function sum () {
-	if (typeof arguments !== 'undefined') {
-		arguments.reduce = [].reduce;
-		return arguments.reduce(function (a, b) {
-			let numerator = function(elementInt) {
-				let elementInt;
-				if (typeOf elementInt === 'number') {
-					return elementInt;
-				}
-				return 0;
-			};
-
-			return (numerator(a)+numerator(b));
-
-			// if (typeof a === 'number') {
-			// 	if (typeof b === 'number') {
-			// 		return a + b;
-			// 	}
-			// }
-		});
+		for (element of array) {
+			if (typeof element === 'number' && minimumnNumer > element) {
+				minimumnNumer = element;
+			}
+		}
+		return minimumnNumer;
 	}
+}
+
+export function max (array) {
+	if (typeof array !== 'undefined') {
+		let minimumnNumer = array[0];
+		let element;
+
+		for (element of array) {
+			if (typeof element === 'number' && minimumnNumer < element) {
+				minimumnNumer = element;
+			}
+		}
+		return minimumnNumer;
+	}
+	return undefined;
+}
+
+function returnNumber (intChecker) {
+	if (typeof intChecker === 'number') {
+		return intChecker;
+	}
+	return 0;
+}
+
+export function sum () {
+	if (typeof arguments !== 'undefined') {
+		let args = Array.from(arguments);
+		let result = 0;
+		let element;
+
+		for (element of args) {
+			result += returnNumber(element);
+		}
+		return result;
+	}
+	return undefined;
 }
 
 console.log(1);
@@ -60,7 +58,7 @@ console.log(1);
 // let testerArray = [2, 3, 5, 7, 9, 11, 'd', 13, 6, 1];
 // let testerArray2;
 
-console.log(sum('a', 5, [1, 5, 8]));
+// console.log(sum('a', 5, [1, 5, 8]));
 
 // console.log(min(testerArray));
 // console.log(max(testerArray));
